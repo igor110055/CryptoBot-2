10e12e6773818565e5b2afe430999ddf22df5a3b
Ol86 <ole.menke@gmail.com>
2022-06-18 02:44:44 +0200

    update

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import mysql.connector
 # Initialize the database
 mydb = mysql.connector.connect(
     host="localhost",
-  user="myusername",
-  password="mypassword",
-  database="mydatabase"
+    user="admin",
+    password="raspberry",
+    database="cryptodata"
 )
+
+mycursor = mydb.cursor()
 
 # Get the asset data
 respToken = requests.get('https://api.kraken.com/0/public/Assets')
@@ -31,6 +33,17 @@ def getValues(value):
     for i in token['result'].keys():
         valueData.append(token['result'][i][value])
     return valueData
+
+def storeAssetToDatabase(key, value, day):
+    querry = "CREATE TABLE assets" + day + " (id INT NOT NULL AUTO_INCREMENT, asset VARCHAR(255), altname VARCHAR(255))"
+    mycursor.execute(querry)
+    for i in range(len(key)):
+        querry = "INSERT INTO assets" + day + "(asset, altname) VALUES (%s, %s)"
+        values = (key[i], value[i])
+        mycursor.execute(querry, values)
+        mydb.commit()
+
+
 
 def storeToCSV(fileName, key, value, request):
     with open(fileName, 'w') as file:
